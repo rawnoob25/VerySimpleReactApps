17697fdf074815e30ec8c46d2af89c5bc0130922
Raw Noob <rawnoob25@gmail.com>
2018-04-11 18:35:31 -0400

    meals with(out) judgment- click does not work

--- App.js
+++ App.js
@@ -26,6 +26,12 @@ function change(e){
   e.target.setAttribute('height','500')
 }
 
+let dessertString = 'Nothing';
+
+function isJudgmental(){
+  dessertString = Math.random()<0.5?'blueberry and acai smoothie':'double fudge brownie';
+}
+
 const catURL = 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg';
 const pic = <img src = {catURL} alt = 'cat' onClick = {change} height = '500'/>
 const dogURL = 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg';
@@ -34,10 +40,23 @@ const goose = 'https://s3.amazonaws.com/codecademy-content/courses/React/react_p
 
 const gooseImg1 = <img src = {goose} alt = 'goose orig'  /> /*gotta put regular js expression inside braces when it's embedded inside of jsx*/ 
 const gooseImg2 = <img src = {goose} alt = 'goose orig' height = '500' />
+
+
+
+const favMeals = (<div>
+  <h1 onClick = {isJudgmental}>Favorite Meals (click for dessert)</h1>
+  <ul>
+    <li>Breakfast: oatmeal, milk and raisins</li>
+    <li>Lunch: tuna sandwich</li>
+    <li>Dinner: Salmon steak and broccoli</li>
+    <li>Pre-bed dessert: {dessertString} </li>
+  </ul>
+</div>);
+
 class App extends Component {
   render() {
     return (
-      <div>{pic}</div>
+      <div>{favMeals}</div>
       /*
       <div>
         <div>{gooseImg1}</div>
