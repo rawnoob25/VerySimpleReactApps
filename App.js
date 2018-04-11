3922fece245080c8141282bee4fedad64d47a178
Raw Noob <rawnoob25@gmail.com>
2018-04-11 19:25:03 -0400

    cleaned up App.js some more and changed a const to a let in meals with(out) judgment

--- App.js
+++ App.js
@@ -5,19 +5,20 @@ import './App.css';
 
 let dessertString = 'Nothing';
 
-function isJudgmental(){
+function isJudgmental() {
   dessertString = Math.random()<0.5?'blueberry and acai smoothie':'double fudge brownie';
 }
 
-const favMeals = (<div>
-  <h1 onClick = {isJudgmental}>Favorite Meals (click for dessert)</h1>
-  <ul>
-    <li>Breakfast: oatmeal, milk and raisins</li>
-    <li>Lunch: tuna sandwich</li>
-    <li>Dinner: Salmon steak and broccoli</li>
-    <li>Pre-bed dessert: {dessertString} </li>
-  </ul>
-</div>
+let favMeals = (
+  <div>
+    <h1 onClick = {isJudgmental}>Favorite Meals (click for dessert)</h1>
+    <ul>
+      <li>Breakfast: oatmeal, milk and raisins</li>
+      <li>Lunch: tuna sandwich</li>
+      <li>Dinner: Salmon steak and broccoli</li>
+      <li>Pre-bed dessert: {dessertString} </li>
+    </ul>
+  </div>
 );
 
 class App extends Component {
