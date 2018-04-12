d8c6966d097a388fd063741539ecaac5ee14bba7
Raw Noob <rawnoob25@gmail.com>
2018-04-12 17:02:27 -0400

    make simple todolist app. get a failed prop type warning on load. unable to edit input field. button does nothing

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
 
-let palindromeStrings = ['able','was','i','ere','i','saw','elba']
+let input = (
+  <div>
+    Add to list: <input id = 'toAdd' type = 'text' name = 'toAdd' value='Something' />
+  </div>
+);
 
-let palindromeStringsList = palindromeStrings.map(e=><li>{e}</li>);
-let theList = <ul>{palindromeStringsList}</ul>
+let items = [];
+
+function add(){
+  let theInput = document.getElementById('toAdd');
+  items.push(theInput.value);
+}
+
+let addBtn = (
+  <div><button onClick = {add}>Add to List</button></div>
+);
+
+let listElements = items.map(e=><li>{e}</li>);
+let listOfItems = <ul>{listElements}</ul>
+
+let container = (
+  <div>
+    <div>{input}</div>
+    <div>{addBtn}</div>
+    <div>{listOfItems}</div>
+  </div>
+);
+
 class App extends Component {
   render() {
-    return theList
+    return container;
   }
 }
 
