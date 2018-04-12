2aafe1c7b5f203a8148c85fedc1fe69c806e8c70
Raw Noob <rawnoob25@gmail.com>
2018-04-12 19:01:04 -0400

    display button and alert on click

--- App.js
+++ App.js
@@ -1,31 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const awesomeQuoteText = 'The world is full of objects, more or less interesting; I do not wish to add anymore';
-const picInfo = {
-  src: 'https://upload.wikimedia.org/wikipedia/en/a/a4/Douglas_Huebler.jpg',
-  title: 'Douglas Huebler'
-};
 
-const quote = (
-  <blockquote>
-    <p>
-      {awesomeQuoteText}
-    </p>
-    <cite>
-      -{picInfo.title}
-    </cite>
-  </blockquote>
-);
+
 
 class App extends Component {
+  msg(){
+    alert('You have been clicked.');
+  }
   render() {
     return (
-      <div>
-        {quote}
-        <img src = {picInfo.src} alt = {picInfo.title} />
-      </div>
-      
+      <button onClick = {this.msg}>Click me!</button>
     );
   }
 }
